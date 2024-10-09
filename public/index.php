bdfd7fa292c5bdce3639340224549b643403a0da
Chenee <cespina@smartvisit.com>
2024-10-09 15:26:51 +0800

    Updated Public/index

--- public/index.php
+++ public/index.php
@@ -2,33 +2,52 @@
 
 use Laminas\Mvc\Application;
 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+error_reporting(E_ALL);
+
+try {
+    /**
+     * This makes our life easier when dealing with paths. Everything is relative
+     * to the application root now.
+     */
+    chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (is_string($path) && __FILE__ !== $path && is_file($path)) {
-        return false;
+    if (php_sapi_name() === 'cli-server') {
+        $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        if (is_string($path) && __FILE__ !== $path && is_file($path)) {
+            return false;
+        }
+        unset($path);
     }
-    unset($path);
-}
 
 // Composer autoloading
-include __DIR__ . '/../vendor/autoload.php';
+    include __DIR__ . '/../vendor/autoload.php';
 
-if (!class_exists(Application::class)) {
-    throw new RuntimeException(
-        "Unable to load application.\n"
-        . "- Type `composer install` if you are developing locally.\n"
-        . "- Type `docker-compose run laminas composer install` if you are using Docker.\n"
-    );
+    if (!class_exists(Application::class)) {
+        throw new RuntimeException(
+            "Unable to load application.\n"
+            . "- Type `composer install` if you are developing locally.\n"
+            . "- Type `docker-compose run laminas composer install` if you are using Docker.\n"
+        );
+    }
+
+    $container = require __DIR__ . '/../config/container.php';
+// Run the application!
+    $container->get('Application')
+              ->run();
+} catch (\Exception $exception) {
+    echo "<pre>";
+    print_r($exception->getMessage());
+    echo "</pre>";
+    echo "<pre>";
+    print_r('***');
+    echo "</pre>";
+    echo "<pre>";
+    print_r($exception->getTraceAsString());
+    echo "</pre>";
+    die();
 }
 
-$container = require __DIR__ . '/../config/container.php';
-// Run the application!
-$container->get('Application')
-          ->run();
+
