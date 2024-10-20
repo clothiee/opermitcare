391a1aa05e2b7dad551af3953a89795d38bc602a
Chenee <cespina@smartvisit.com>
2024-10-21 04:21:11 +0800

    Add resident

--- webpack/js/modules/layout-settings.js
+++ webpack/js/modules/layout-settings.js
@@ -4,12 +4,28 @@ export class LayoutSettingsModule {
     };
 
     initialize() {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 300) {
-                return $('.navigation').addClass('navigation--sticky');
-            }
+        let addEventListener = function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 300) {
+                    return $('.navigation').addClass('navigation--sticky');
+                }
 
-            return $('.navigation').removeClass('navigation--sticky');
-        });
+                return $('.navigation').removeClass('navigation--sticky');
+            });
+
+            $(document).on('click', '.dashboard__list-item, .js-see-tickets', function () {
+                const targetTab = $(this).attr('data-tab');
+
+                $(`.dashboard__tab`).removeClass('active');
+                $(`.dashboard__tab[data-tab="${targetTab}"]`).addClass('active');
+            });
+
+            $(document).on('click', '.dashboard__list-item', function () {
+                $(`.dashboard__list-item`).removeClass('active');
+                $(this).addClass('active');
+            });
+        };
+
+        return addEventListener();
     }
 }
